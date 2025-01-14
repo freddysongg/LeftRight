317612d9f40c41a21e49e62721ff7f3864273759
Freddy Song <fredsong99@gmail.com>
2025-01-13 18:55:55 -0800

    feat: vercel analytics

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,13 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
+import { Analytics } from '@vercel/analytics/react';
 import './index.css';
 import App from './App.tsx';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
+    <Analytics />
     <BrowserRouter>
       <App />
     </BrowserRouter>
